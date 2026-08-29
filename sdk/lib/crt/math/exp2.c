2da6ac0b36b6411f211bb6a10ade08b5d794f57c
sjljeh <282129024+sjljeh@users.noreply.github.com>
2026-08-29 17:40:12 -0400

    [CRT] Remove Clang hacks

--- sdk/lib/crt/math/exp2.c
+++ sdk/lib/crt/math/exp2.c
@@ -7,7 +7,5 @@ __cdecl
 exp2(
     _In_ double x)
 {
-    /* This below avoids clang to optimize our pow call to exp2 */
-    static const double TWO = 2.0;
-    return pow(TWO, x);
+    return pow(2.0, x);
 }
