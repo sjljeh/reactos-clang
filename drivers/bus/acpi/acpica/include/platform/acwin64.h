7a1973d3485b027452fcff063ce9d9baa7bbfc97
sjljeh <282129024+sjljeh@users.noreply.github.com>
2026-08-29 18:19:05 -0400

    [ACPI] Fix Clang compilation with ACPI spinlock

--- drivers/bus/acpi/acpica/include/platform/acwin64.h
+++ drivers/bus/acpi/acpica/include/platform/acwin64.h
@@ -136,8 +136,8 @@
 \
     if ((FacsPtr) != 0) \
     { \
-        UINT32 compare, prev, newval; \
-        UINT32* lock = &((FacsPtr)->GlobalLock); \
+        long compare, prev, newval; \
+        volatile long *lock = ACPI_CAST_PTR (volatile long, &((FacsPtr)->GlobalLock)); \
         do \
         { \
             compare = *lock; \
@@ -155,7 +155,7 @@
 \
     if ((FacsPtr) != 0) \
     { \
-        pending = InterlockedAnd(&(FacsPtr)->GlobalLock, ~3) & 1; \
+        pending = InterlockedAnd(ACPI_CAST_PTR (volatile long, &((FacsPtr)->GlobalLock)), ~3) & 1; \
     } \
     (Pnd) = pending; \
 }
