c2baa4ee55a343c80fb2f0ddb83d6e028a70b51e
sjljeh <282129024+sjljeh@users.noreply.github.com>
2026-08-29 17:33:53 -0400

    [MVDM] Remove Clang hack

--- subsystems/mvdm/asm16.cmake
+++ subsystems/mvdm/asm16.cmake
@@ -100,11 +100,7 @@ function(add_asm16_bin _target _binary_file _base_address)
     ##
     ## All this part is the same as CreateBootSectorTarget
     ##
-    if(CMAKE_C_COMPILER_ID STREQUAL "Clang")
-        set(_no_std_includes_flag "-nostdinc")
-    else()
-        set(_no_std_includes_flag "/X")
-    endif()
+    set(_no_std_includes_flag "/X")
 
     add_custom_command(
         OUTPUT ${_preprocessed_asm_file}
