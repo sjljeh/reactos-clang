447b7d9ad7881edd5b1dd26596e794250813cf95
sjljeh <282129024+sjljeh@users.noreply.github.com>
2026-09-12 13:42:17 -0400

    Revert "[HAL:APIC] Extend BSP timer calibration window"
    This reverts commit 9f4cc2326432d1d4a8690472ab5ee17c26686d16.

--- hal/halx86/apic/tsc.h
+++ hal/halx86/apic/tsc.h
@@ -2,13 +2,7 @@
 #ifndef _TSC_H_
 #define _TSC_H_
 
-/*
- * Four 1024-Hz RTC samples cover less than three milliseconds.  A single
- * delayed RTC delivery can therefore skew both TSC and LAPIC calibration by
- * tens of percent on an oversubscribed SMP guest.  Cover roughly 124 ms so
- * interrupt-delivery jitter is a small part of the measured interval.
- */
-#define NUM_SAMPLES 128
+#define NUM_SAMPLES 4
 #define MSR_RDTSC 0x10
 #define RTC_MODE 6 /* Mode 6 is 1024 Hz */
 #define SAMPLE_FREQUENCY ((32768 << 1) >> RTC_MODE)
