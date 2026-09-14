cda5e1faaf02de0ed036a122bc6cf41d7d32ae1c
sjljeh <282129024+sjljeh@users.noreply.github.com>
2026-09-14 09:10:47 -0400

    [NTOS:MM] Avoid unlocked pool page validation

--- ntoskrnl/mm/ARM3/expool.c
+++ ntoskrnl/mm/ARM3/expool.c
@@ -2554,7 +2554,6 @@ ExAllocatePoolWithTag(IN POOL_TYPE PoolType,
     //
     // And return the pool allocation
     //
-    ExpCheckPoolBlocks(Entry);
     Entry->PoolTag = Tag;
     return POOL_FREE_BLOCK(Entry);
 }
