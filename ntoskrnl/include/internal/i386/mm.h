9ebd83733831982f9efb525ef64387aa773b153a
Justin Miller <justin.miller@reactos.org>
2026-09-13 20:06:05 -0700

    [NTOS:MM] Restore the original characters of the x86 paging comment

--- ntoskrnl/include/internal/i386/mm.h
+++ ntoskrnl/include/internal/i386/mm.h
@@ -140,7 +140,7 @@
 #define MI_WRITE_VALID_PPE MI_WRITE_VALID_PTE
 
 /*  Translating virtual addresses to physical addresses
-        (See: "Intelï¿½ 64 and IA-32 Architectures Software Developerï¿½s Manual
+        (See: "Intel® 64 and IA-32 Architectures Software Developer’s Manual
               Volume 3A: System Programming Guide, Part 1, CHAPTER 4 PAGING")
     Page directory (PD) and Page table (PT) definitions
     Page directory entry (PDE) and Page table entry (PTE) definitions
