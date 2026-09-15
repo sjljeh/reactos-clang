d3e24859578385fc377816e38139393907bbc8ae
sjljeh <282129024+sjljeh@users.noreply.github.com>
2026-09-14 21:12:20 -0400

    Revert "[WIN32K:NTUSER] Publish top-level clipping atomically"
    This reverts commit 471918ccbbd6bef02ff80742ed6f97280d723b89.

--- win32ss/user/ntuser/dce.h
+++ win32ss/user/ntuser/dce.h
@@ -31,8 +31,6 @@ typedef struct tagDCE
     struct _MONITOR* pMonitor;
 } DCE, *PDCE;
 
-typedef struct _DCE_LAYOUT_LOCK DCE_LAYOUT_LOCK, *PDCE_LAYOUT_LOCK;
-
 /* internal DCX flags, see psdk/winuser.h for the rest */
 #define DCX_DCEEMPTY        0x00000800
 #define DCX_DCEBUSY         0x00001000
@@ -49,8 +47,6 @@ HWND FASTCALL IntWindowFromDC(HDC hDc);
 void FASTCALL DceFreeDCE(PDCE dce, BOOLEAN Force);
 void FASTCALL DceEmptyCache(void);
 VOID FASTCALL DceResetActiveDCEs(PWND Window);
-PDCE_LAYOUT_LOCK FASTCALL DceBeginLayoutLock(VOID);
-VOID FASTCALL DceEndLayoutLock(PWND Window, PDCE_LAYOUT_LOCK Lock);
 void FASTCALL DceFreeClassDCE(PDCE);
 HWND FASTCALL UserGethWnd(HDC,PWNDOBJ*);
 void FASTCALL DceFreeWindowDCE(PWND);
