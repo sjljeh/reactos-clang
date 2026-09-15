0c30d19f4331d1f0bd3ec5f7f4cbbe1dfef189c9
sjljeh <282129024+sjljeh@users.noreply.github.com>
2026-09-15 10:16:23 -0400

    Revert "[BOOTDATA] Embed the live RAM disk for serial debugging"
    This reverts commit de3c252b33fc528aea70642cad4fdaf0a5ba0c3e.

--- sdk/cmake/CMakeMacros.cmake
+++ sdk/cmake/CMakeMacros.cmake
@@ -408,7 +408,7 @@ function(create_iso_lists)
         DESTINATION reactos
         NO_CAB FOR bootcd regtest)
 
-if(TRUE) ## Include the RAMDISK ISO for out-of-band serial debugging
+if(FALSE) ## Disabled until we want a RAMDISK ISO
     # Add the LiveImage into the BootCD
     add_cd_file(
         TARGET livecd
